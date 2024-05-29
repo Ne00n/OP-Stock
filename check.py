0599c44f68fd0e992f55b251132ca698410c5847
Neoon <neo@tiefkuehler-madness.me>
2024-05-29 16:27:02 +0200

    add example json

--- check.py
+++ check.py
@@ -19,10 +19,12 @@ for region,data in response['response'].items():
             if country['available_sizes']:
                 print(f"{country['country']} is available")
                 if os.path.isfile(country['location_id']): continue
+                #OneProvider has not always the cheapest package available in a certain location, so we try to buy the smallest one
                 #1055 is debian 12 btw
                 data = {"location_id": country['id'],"instance_size": min(country['available_sizes']),"template": "1055","hostname": "seek.code.run.com"}
-                response = requests.post('https://api.oneprovider.com/vm/create', headers=headers, json=data)
-                with open(country['location_id'], 'w') as file: file.write("purchased")
+                req = requests.post('https://api.oneprovider.com/vm/create', headers=headers, json=data)
+                if req.status_code == 200:
+                    with open(country['location_id'], 'w') as file: file.write(f"purchased package {min(country['available_sizes'])}")
             else:
                 print(f"{country['country']} is not available")
 
